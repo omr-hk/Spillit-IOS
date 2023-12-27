44e8d7759a1525c7c02c4659073184b6973a8020
omr-hk <work.omrhk@gmail.com>
2023-12-27 22:50:00 +0530

    Initial Commit

---
+++ Spillit-IOS/Spillit_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spillit_IOSApp.swift
+//  Spillit-IOS
+//
+//  Created by Omar Hassan on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Spillit_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
